723f3a1b9a4683b5c889cc9d393865252fb289b3
Zee1234 <zeeatgit@gmail.com>
2016-08-15 16:58:29 -0500

    Backup for load of LDH outside of initialization

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
-g_Plugin = nil --I know this does nothing, it's a note.
-
-
 function Initialize(a_Plugin)
   a_Plugin:SetName("LibDependHandler")
   a_Plugin:SetVersion(1.0)
 	
+  
+  cPluginManager:AddHook(cPluginManager.HOOK_PLUGINS_LOADED, LDHBroadcast)
+  cRoot:Get():GetDefaultWorld():ScheduleTask(1,LDHBroadcast)
+  
+  
   g_Plugin = a_Plugin
   LOG("Initialized LibDependHandler v." .. a_Plugin:GetVersion())
   return true
@@ -26,4 +28,10 @@ end
 
 function verify(a_name)
   return not cPluginManager:ForEachPlugin(function(a_plugin) if a_plugin:GetName() == a_name then return true end end) and cPluginManager:CallPlugin(a_name,"LDHVerify")
+end
+
+function LDHBroadcast()
+  if LDHSent then return; end
+  cPluginManager:ForEachPlugin(function(a_plugin) cPluginManager:CallPlugin(a_plugin:GetName(),"LDHSetup") end)
+  LDHSent = true
 end
