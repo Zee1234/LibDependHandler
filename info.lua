bb0596b51ce5306839c888b295f8ce0027cbf063
Zee1234 <zeeatgit@gmail.com>
2016-08-23 21:30:16 -0500

    Info.lua

---
+++ info.lua
@@ -0,0 +1,12 @@
+g_PluginInfo =
+{
+	Name = "LibDependHandler",
+	Date = "2016-08-23",
+	Description = "A small library to make dependencies in Cuberite not stupid.",
+
+	-- The following members will be documented in greater detail later:
+	AdditionalInfo = {},
+	Commands = {},
+	ConsoleCommands = {},
+	Permissions = {},
+}
